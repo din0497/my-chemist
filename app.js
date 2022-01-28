1ea197a62f7c578bbf5c90e5593f7906cad19c56
Rivojiddin Abdunazarov <68281739+din0497@users.noreply.github.com>
2022-01-28 23:06:20 +0500

    server

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const http = require('http');
 
-function rqListener(req, res) {
 
-}
+const server = http.createServer((req, res) => {
+ console.log(req);
+});
 
-http.createServer(rqListener);
+server.listen(3000);
