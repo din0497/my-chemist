ebfa79e941b117563e66710fce17fce9a0f40eb9
Rivojiddin Abdunazarov <68281739+din0497@users.noreply.github.com>
2022-01-29 22:15:54 +0500

    connecting req and res

--- app.js
+++ app.js
@@ -3,13 +3,18 @@ const http = require("http");
 const server = http.createServer((req, res) => {
   const url = req.url;
   if (url === "/me") {
-    res.setHeader("Content-Type", "text/html");
     res.write("<html>");
-    res.write("<head><title> My first node </title></head?");
-    res.write("<body><h1> Hello from my Node js</h1></body>");
+    res.write("<head><title> me page</title></head?");
+    res.write("<body><h1> You are in me directory</h1></body>");
     res.write("</html>");
     res.end();
   }
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title> My first node </title></head?");
+  res.write("<body><h1> Hello from my Node js</h1></body>");
+  res.write("</html>");
+  res.end();
 });
 
 server.listen(3000);
