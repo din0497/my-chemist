9f4bcf98eb3b11a3c7d6d431fb471be704a8b1ac
Rivojiddin Abdunazarov <68281739+din0497@users.noreply.github.com>
2022-01-29 21:50:40 +0500

    event loop

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
-const http = require('http');
-
+const http = require("http");
 
 const server = http.createServer((req, res) => {
- console.log(req);
- process.exit()
+  console.log(req);
 });
 
 server.listen(3000);
