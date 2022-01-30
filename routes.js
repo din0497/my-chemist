924c871e0c680c3ff9a1005c53e57ccd30c137ce
Rivojiddin Abdunazarov <68281739+din0497@users.noreply.github.com>
2022-01-30 21:17:48 +0500

    Assignment

--- routes.js
+++ routes.js
@@ -54,13 +54,19 @@ const requestHandler = (req, res) => {
     return res.end();
   }
 
-  if(url === '/create-user' && method === 'POST'){
-      const body = []
+  if (url === "/create-user" && method === "POST") {
+    const body = [];
 
-      req.on('data',(chunk)=>{
-          console.log(chunk);
-          body.push(chunk)
-      })
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      console.log({ parsedBody, message });
+    });
   }
 };
 
