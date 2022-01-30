c1bbe315ee701ecbfe4bea66932cfe9fa1622e3b
Rivojiddin Abdunazarov <68281739+din0497@users.noreply.github.com>
2022-01-30 11:00:21 +0500

    redirecting user

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   const url = req.url;
   if (url === "/") {
     res.write("<html>");
     res.write("<head><title> me page</title></head?");
-    res.write("<body><form action='/message' method='POST'><input type='text' name='message'/><button type='submit'>submit</button</form></body>");
+    res.write(
+      "<body><form action='/message' method='POST'><input type='text' name='message'/><button type='submit'>submit</button</form></body>"
+    );
     res.write("</html>");
     return res.end();
+  }
+  if (url === "/message" && method === "POST") {
+    fs.writeFile("message.txt", "DUMMY");
   }
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
