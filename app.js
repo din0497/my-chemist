8361fd7d7051c717442c790d1d2b5ad2c27e91cf
Rivojiddin Abdunazarov <68281739+din0497@users.noreply.github.com>
2022-01-31 22:17:46 +0500

    express assignment

--- app.js
+++ app.js
@@ -13,17 +13,29 @@
 // app.listen(3000);
 
 const express = require("express");
+const req = require("express/lib/request");
 
 const app = express();
 
-app.use('/users',(req, res, next) => {
-    console.log("I am assignment");
-    res.send("<ul><li>users</li></ul>");
-  });
+// app.use((req, res, next) => {
+//     console.log("I am assignment");
+//     next()
+//   });
 
-app.use('/' ,(req, res, next) => {
-  console.log("I am assignment2");
-  res.send("<h1>Hello world!</h1>");
-});
+// app.use((req, res, next) => {
+//   console.log("I am assignment2");
+//   res.send("<h1>Hello world!</h1>");
+// });
+app.use('/users',(req,res, next) => {
+    console.log('/users middleware');
+    res.send("<p>/////users</p>")
+})
+
+
+app.use('/',(req,res, next) => {
+    console.log('/ middleware');
+    res.send("<p>/////</p>")
+})
+
 
 app.listen(3000);
