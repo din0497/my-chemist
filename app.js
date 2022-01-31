483181af019d58a85fb16f2e809f8893e2658274
Rivojiddin Abdunazarov <68281739+din0497@users.noreply.github.com>
2022-01-31 11:57:09 +0500

    express

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const http = require("http");
 
 const express = require('express')
 
-const routes = require('./routes')
+const app = express();
 
-const server = http.createServer(routes);
+const server = http.createServer(app);
 
 server.listen(3000);
