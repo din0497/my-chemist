f01722c831073381d7b3830aaaec12a2f9e59c1f
Rivojiddin Abdunazarov <68281739+din0497@users.noreply.github.com>
2022-01-31 22:57:09 +0500

    express routing

--- app.js
+++ app.js
@@ -1,43 +1,16 @@
 const express = require("express");
-const bodyParser = require('body-parser')
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended:false}))
+const adminRoutes = require("./routes/admin");
 
-app.use("/", (req, res, next) => {
-  next();
-});
-app.use("/add-product", (req, res, next) => {
-  res.send(
-    "<form action='/product' method='POST'><input type='text' name='title'/><button type='submit'>Submit</button></form>"
-  );
-});
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.post("/product", (req, res, next) => {
- console.log(req.body)
-    res.redirect("/");
-});
+app.use(adminRoutes);
 
 app.use("/", (req, res, next) => {
   res.send("<h1>Hello from Express!</h1>");
 });
 
 app.listen(3000);
-
-// const express = require("express");
-// const req = require("express/lib/request");
-
-// const app = express();
-
-// app.use('/users',(req,res, next) => {
-//     console.log('/users middleware');
-//     res.send("<p>/////users</p>")
-// })
-
-// app.use('/',(req,res, next) => {
-//     console.log('/ middleware');
-//     res.send("<p>/////</p>")
-// })
-
-// app.listen(3000);
