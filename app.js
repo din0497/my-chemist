2c09b8bccb7175cdf6124a8405f927ea6c08ab6d
Rivojiddin Abdunazarov <68281739+din0497@users.noreply.github.com>
2022-01-31 21:14:41 +0500

    express

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const http = require("http");
 
-const express = require('express')
+const express = require("express");
 
 const app = express();
+
+app.use((req, res, next) => {
+    console.log('in the middleware');
+    next()
+});
+app.use((req, res, next) => {
+    console.log('in another middleware');
+    res.send('<h1>Hello from Express!</h1>')
+});
 
 const server = http.createServer(app);
 
