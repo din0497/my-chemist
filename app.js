e188e2788621ca34ecbcbeaf1ac6651be6360663
Rivojiddin Abdunazarov <68281739+din0497@users.noreply.github.com>
2022-02-01 16:09:58 +0500

    html pages style

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const path = require('path')
-const rootDir = require('./util/path')
-
+const path = require("path");
+const rootDir = require("./util/path");
 
 const app = express();
 
@@ -10,6 +9,7 @@ const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname,'public')))
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
