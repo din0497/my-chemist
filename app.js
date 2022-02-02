523089365c56bef0a8ab33b13498d4588f395805
Rivojiddin Abdunazarov <68281739+din0497@users.noreply.github.com>
2022-02-02 21:23:06 +0500

    pug syntax

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.set('view engine', 'pug')
-app.set('views', 'viewa')
+app.set('views', 'views')
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
